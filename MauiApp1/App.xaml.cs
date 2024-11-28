09a205e515533efdd1dea4bd8eca2ba10831c94b
OliverHaffenrichter <haffenrichter@gmail.com>
2024-11-28 14:39:47 +0100

    Dansk text

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace MauiApp1
+﻿using System.Globalization;
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            var culture = new CultureInfo("da-DK");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
